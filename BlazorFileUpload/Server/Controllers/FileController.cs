15d63270a505bd0194f095974ebebf621cd65e24
Patrick <patrick@godknows.de>
2022-07-22 23:13:51 +0200

    Add project files.

---
+++ BlazorFileUpload/Server/Controllers/FileController.cs
@@ -0,0 +1,45 @@
+﻿using BlazorFileUpload.Shared;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+
+namespace BlazorFileUpload.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FileController : ControllerBase
+    {
+        private readonly IWebHostEnvironment _env;
+
+        public FileController(IWebHostEnvironment env)
+        {
+            _env = env;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<List<UploadResult>>> UploadFile(List<IFormFile> files)
+        {
+            List<UploadResult> uploadResults = new List<UploadResult>();
+
+            foreach(var file in files)
+            {
+                var uploadResult = new UploadResult();
+                string trustedFileNameForFileStorage;
+                var untrustedFileName = file.FileName;
+                uploadResult.FileName = untrustedFileName;
+                var trustedFileNameForDisplay = WebUtility.HtmlEncode(untrustedFileName);
+
+                trustedFileNameForFileStorage = Path.GetRandomFileName();
+                var path = Path.Combine(_env.ContentRootPath, "uploads", trustedFileNameForFileStorage);
+
+                await using FileStream fs = new(path, FileMode.Create);
+                await file.CopyToAsync(fs);
+
+                uploadResult.StoredFileName = trustedFileNameForFileStorage;
+                uploadResults.Add(uploadResult);
+            }
+
+            return Ok(uploadResults);
+        }
+    }
+}
---
+++ BlazorFileUpload/Shared/UploadResult.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorFileUpload.Shared
+{
+    public class UploadResult
+    {
+        public string? FileName { get; set; }
+        public string? StoredFileName { get; set; }
+    }
+}
